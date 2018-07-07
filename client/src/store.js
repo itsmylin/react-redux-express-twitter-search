a41f267d54dcc099588ca5ae66fa50e6a6d80a84
itsmylin <mylin651@gmail.com>
2018-07-07 03:54:33 -0700

    Downgraded redux and fix api bug

--- client/src/store.js
+++ client/src/store.js
@@ -1,18 +1,7 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
-const initialState = {};
-
-const middleware = [thunk];
-
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
