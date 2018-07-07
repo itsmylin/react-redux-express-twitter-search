53ff2e2aa5065bff6fe39c42b9cf7cda78e07703
itsmylin <mylin651@gmail.com>
2018-07-07 02:57:28 -0700

    add script in jsx

--- client/src/components/search/TweetEmbed.js
+++ client/src/components/search/TweetEmbed.js
@@ -1,10 +1,37 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
+const callbacks = [];
+
+function addScript(src, cb) {
+  if (callbacks.length === 0) {
+    callbacks.push(cb);
+    var s = document.createElement("script");
+    s.setAttribute("src", src);
+    s.onload = () => callbacks.forEach(cb => cb());
+    document.body.appendChild(s);
+  } else {
+    callbacks.push(cb);
+  }
+}
+
 class TweetEmbed extends Component {
   loadTweetForProps(props) {
-    const { options, id } = props;
-    window.twttr.widgets.createTweet(id, document.getElementById(id), options);
+    const renderTweet = () => {
+      window.twttr.ready().then(({ widgets }) => {
+        const { options, id } = props;
+        widgets.createTweetEmbed(id, document.getElementById(id), options);
+      });
+    };
+
+    if (!(window.twttr && window.twttr.ready)) {
+      const isLocal = window.location.protocol.indexOf("file") >= 0;
+      const protocol = isLocal ? this.props.protocol : "";
+
+      addScript(`${protocol}//platform.twitter.com/widgets.js`, renderTweet);
+    } else {
+      renderTweet();
+    }
   }
   componentDidMount() {
     this.loadTweetForProps(this.props);
@@ -20,7 +47,8 @@ TweetEmbed.propTypes = {
 };
 
 TweetEmbed.defaultProps = {
-  options: {}
+  options: {},
+  protocol: "https"
 };
 
 export default TweetEmbed;
