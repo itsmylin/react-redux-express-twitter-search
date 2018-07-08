687e99bd0bac64b2a8c7cae0f4e1b6f1e29c4ead
itsmylin <mylin651@gmail.com>
2018-07-08 01:01:58 -0700

    deploy to heroku

--- client/src/components/search/TweetEmbed.js
+++ client/src/components/search/TweetEmbed.js
@@ -1,37 +1,18 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-const callbacks = [];
-
-function addScript(src, cb) {
-  if (callbacks.length === 0) {
-    callbacks.push(cb);
-    var s = document.createElement("script");
-    s.setAttribute("src", src);
-    s.onload = () => callbacks.forEach(cb => cb());
-    document.body.appendChild(s);
-  } else {
-    callbacks.push(cb);
-  }
-}
-
 class TweetEmbed extends Component {
   loadTweetForProps(props) {
     const renderTweet = () => {
-      window.twttr.ready().then(({ widgets }) => {
-        const { options, id } = props;
-        widgets.createTweetEmbed(id, document.getElementById(id), options);
-      });
+      const { options, id } = props;
+      window.twttr.widgets.createTweetEmbed(
+        id,
+        document.getElementById(id),
+        options
+      );
     };
-
-    if (!(window.twttr && window.twttr.ready)) {
-      const isLocal = window.location.protocol.indexOf("file") >= 0;
-      const protocol = isLocal ? this.props.protocol : "";
-
-      addScript(`${protocol}//platform.twitter.com/widgets.js`, renderTweet);
-    } else {
-      renderTweet();
-    }
+    window.twttr.widgets.load();
+    renderTweet();
   }
   componentDidMount() {
     this.loadTweetForProps(this.props);
